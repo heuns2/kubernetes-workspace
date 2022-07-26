d7c9033667daca5b8686983bbda2fc84d701cc90
leedh <ehdgus6600@gmail.com>
2022-07-26 16:04:31 +0900

    git commit

--- aws-terraform/infra-create.tf
+++ aws-terraform/infra-create.tf
@@ -171,3 +171,60 @@ resource "aws_eip" "bastion_2a" {
       "Name" = "eip-test-bastion"
     }
 }
+
+# Sharded Filesystem PVC 용도의 EFS 정의
+resource "aws_efs_file_system" "filesystem" {
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+
+  tags = {
+    Name = "test-efs"
+  }
+}
+
+
+resource "aws_efs_access_point" "test" {
+  file_system_id = aws_efs_file_system.filesystem.id
+}
+
+resource "aws_efs_file_system_policy" "policy" {
+  file_system_id = aws_efs_file_system.filesystem.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "Policy01",
+    "Statement": [
+        {
+            "Sid": "Statement",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Resource": "${aws_efs_file_system.filesystem.arn}",
+            "Action": [
+                "elasticfilesystem:ClientMount",
+                "elasticfilesystem:ClientRootAccess",
+                "elasticfilesystem:ClientWrite"
+            ],
+            "Condition": {
+                "Bool": {
+                    "aws:SecureTransport": "false"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_efs_mount_target" "private-1" {
+  file_system_id = aws_efs_file_system.filesystem.id
+  subnet_id      = aws_subnet.private[0].id
+}
+
+resource "aws_efs_mount_target" "private-2" {
+  file_system_id = aws_efs_file_system.filesystem.id
+  subnet_id      = aws_subnet.private[1].id
+}
